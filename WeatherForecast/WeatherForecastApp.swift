601e139e41e564814d52f6d568070c85f9b34cc1
James Kerrane <hi@jameskerrane.com>
2024-06-04 09:31:19 -0600

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by James Kerrane on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
